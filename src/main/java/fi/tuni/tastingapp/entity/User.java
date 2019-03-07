146facd359ce4c87a32002a55c9c565e193a8974
Teemu <teemu.salminen96@gmail.com>
2019-03-07 18:48:23 +0200

    Added basic attributes to User entity.

--- src/main/java/fi/tuni/tastingapp/entity/User.java
+++ src/main/java/fi/tuni/tastingapp/entity/User.java
@@ -1,5 +1,36 @@
 package fi.tuni.tastingapp.entity;
 
 public class User {
+	
+	private long id;
+	
+	private String username;
+	
+	private String password;
+	
+	public User() {}
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
 }
