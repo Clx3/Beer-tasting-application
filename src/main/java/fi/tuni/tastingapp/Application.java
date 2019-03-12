8e09aa3316b27029fc74d6b7e0c4bd7b5b3ff4f9
Teemu <teemu.salminen96@gmail.com>
2019-03-12 13:30:10 +0200

    Added specified component scanning to Application.
    - I am not 100% sure is the component scan needed for all the packages
    that it scans currently, but for now it is like that.
    
    - This could also be configured in the xml files but lets do it this way
    for the beginning.

--- src/main/java/fi/tuni/tastingapp/Application.java
+++ src/main/java/fi/tuni/tastingapp/Application.java
@@ -3,8 +3,15 @@ package fi.tuni.tastingapp;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"fi.tuni.tastingapp",
+		"fi.tuni.tastingapp.configuration",
+		"fi.tuni.tastingapp.controller",
+		"fi.tuni.tastingapp.entity",
+		"fi.tuni.tastingapp.repository"})
 public class Application {
 	
     public static void main(String[] args) {
