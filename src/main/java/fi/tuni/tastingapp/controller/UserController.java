0bd9dfa60a735241b185b46512cf8ad491decb6b
Teemu <teemu.salminen96@gmail.com>
2019-03-12 13:06:58 +0200

    Made UserController into RestController.
    - Also added first method, but it currently returns null. Needs to be
    implemented later.

--- src/main/java/fi/tuni/tastingapp/controller/UserController.java
+++ src/main/java/fi/tuni/tastingapp/controller/UserController.java
@@ -1,5 +1,14 @@
 package fi.tuni.tastingapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+			
+	@RequestMapping("users/")
+	public String getAllUsers() {
+		return null;
+	}
 
 }
