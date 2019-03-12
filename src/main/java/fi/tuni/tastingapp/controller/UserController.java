66a224b9ca912fa357f001a384a86a56f1486dab
Teemu <teemu.salminen96@gmail.com>
2019-03-12 13:35:01 +0200

    Made UserController to use UserRepository with database stuff.
    - UserController now has first implemented method for getting all the
    users out form the database, and it uses the UserRepository interface to
    do it.

--- src/main/java/fi/tuni/tastingapp/controller/UserController.java
+++ src/main/java/fi/tuni/tastingapp/controller/UserController.java
@@ -1,14 +1,23 @@
 package fi.tuni.tastingapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.tuni.tastingapp.entity.User;
+import fi.tuni.tastingapp.repository.UserRepository;
+
 @RestController
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepo;
 			
 	@RequestMapping("users/")
-	public String getAllUsers() {
-		return null;
+	public List<User> getAllUsers() {
+		return userRepo.findAll();
 	}
 
 }
--- src/main/java/fi/tuni/tastingapp/repository/UserRepository.java
+++ src/main/java/fi/tuni/tastingapp/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package fi.tuni.tastingapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fi.tuni.tastingapp.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	
+	public List<User> findAll();
 
 }
