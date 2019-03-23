d2ccf3b5d9a695d453ac43ffed9e5089b5c1676a
Teemu <teemu.salminen96@gmail.com>
2019-03-23 19:14:01 +0200

    Modify BeerAndTastingSessionRepository
    - Added a query to get beerIds by tastingSessionId.

--- src/main/java/fi/tuni/tastingapp/repository/BeerAndTastingSessionRepository.java
+++ src/main/java/fi/tuni/tastingapp/repository/BeerAndTastingSessionRepository.java
@@ -1,9 +1,16 @@
 package fi.tuni.tastingapp.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import fi.tuni.tastingapp.entity.BeerAndTastingSession;
 
 public interface BeerAndTastingSessionRepository extends CrudRepository<BeerAndTastingSession, Long> {
-
+	
+	List<BeerAndTastingSession> findAllByTastingSessionId(long tastingSessionId);
+	
+	@Query(value = "SELECT beerId FROM beerAndTastingSession WHERE tastingSessionId LIKE(?1)", nativeQuery = true)
+	Long[] findAllBeerIdsByTastingSessionId(long tastingSessionId);
 }
