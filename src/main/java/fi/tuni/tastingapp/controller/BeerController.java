51a78aff266f0d71fe5545e2733dac34dfa3ed77
Joonas Salojärvi <salojarvi.joonas@gmail.com>
2019-03-26 01:28:36 +0200

    Add request for one beer by ID
    RequestMapping method for requesting one beer.

--- src/main/java/fi/tuni/tastingapp/controller/BeerController.java
+++ src/main/java/fi/tuni/tastingapp/controller/BeerController.java
@@ -1,13 +1,11 @@
 package fi.tuni.tastingapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -16,10 +14,10 @@ import fi.tuni.tastingapp.repository.BeerRepository;
 
 @RestController
 public class BeerController {
-	
+
 	@Autowired
 	private BeerRepository beerRepository;
-	
+
 	@RequestMapping(value = "beers/{beerIds}", method = RequestMethod.GET)
 	public List<Beer> getBeersByIds(@PathVariable List<Long> beerIds) {
 		return beerRepository.findByIdIn(beerIds);
@@ -29,10 +27,27 @@ public class BeerController {
 	public List<Beer> getAllBeers() {
 		return beerRepository.findAll();
 	}
-	
+
 	@RequestMapping(value = "beers/add", method = RequestMethod.PUT)
 	public Beer addBeer(@RequestBody Beer beer) throws JsonProcessingException {
 		return beerRepository.save(beer);
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Beer getBeerById(@PathVariable long id) throws BeerNotFoundException {
+		Optional<Beer> beer = beerRepository.findById(id);
+		if (beer.isPresent()) {
+			return beer.get();
+		} else {
+			throw new BeerNotFoundException();
+		}
+
+	}
+}
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	class BeerNotFoundException extends Exception{
+
+	public BeerNotFoundException() {
+		super("Beer with requested id was not found from database");
+	}
 }
