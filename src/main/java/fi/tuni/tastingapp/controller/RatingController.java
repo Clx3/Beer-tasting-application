e57a232bc6a2d5b90382287b27547f286738a3af
Teemu <teemu.salminen96@gmail.com>
2019-04-09 16:02:58 +0300

    Added request mapping method for receiving all ratings

--- src/main/java/fi/tuni/tastingapp/controller/RatingController.java
+++ src/main/java/fi/tuni/tastingapp/controller/RatingController.java
@@ -1,8 +1,13 @@
 package fi.tuni.tastingapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.tuni.tastingapp.entity.Rating;
 import fi.tuni.tastingapp.repository.RatingRepository;
 
 @RestController
@@ -10,5 +15,10 @@ public class RatingController {
 
 	@Autowired
 	private RatingRepository ratingRepository;
+	
+	@RequestMapping(value = "rating", method = RequestMethod.GET)
+	public List<Rating> getAll() {
+		return ratingRepository.findAll();
+	}
 	
 }
--- src/main/java/fi/tuni/tastingapp/repository/RatingRepository.java
+++ src/main/java/fi/tuni/tastingapp/repository/RatingRepository.java
@@ -1,9 +1,13 @@
 package fi.tuni.tastingapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fi.tuni.tastingapp.entity.Rating;
 
 public interface RatingRepository extends CrudRepository<Rating, Long> {
+	
+	public List<Rating> findAll();
 
 }
