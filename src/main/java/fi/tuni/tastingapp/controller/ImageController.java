694336deeab62429da9f51235360d0ac15a13777
Teemu <teemu.salminen96@gmail.com>
2019-04-14 14:32:07 +0300

    ImageUpload WIP

--- src/main/java/fi/tuni/tastingapp/controller/ImageController.java
+++ src/main/java/fi/tuni/tastingapp/controller/ImageController.java
@@ -5,11 +5,18 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
 
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,23 +29,57 @@ import fi.tuni.tastingapp.repository.StorageService;
 @RestController
 public class ImageController {
 	
-    private static final String IMG_FOLDER_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\";
+	/*
+	 * TODO: Implement this stuff in a better way. Currently super messy.
+	 */
 	
-	@RequestMapping(value = "imageUpload", method = RequestMethod.POST)
-	public String uploadImage(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+    private static final Path IMG_FOLDER_PATH = Paths.get(System.getProperty("user.dir") + "/src/main/resources/static/images/");
+	
+	@RequestMapping(value = "images/upload", method = RequestMethod.POST)
+	public String uploadImage(@RequestParam("file") MultipartFile file,
+			@RequestParam(required = true) long beerId) throws IllegalStateException, IOException {
+		
 		/* Converting to .png if not already */
-		System.out.println(file.getContentType());
 		if(!file.getContentType().equalsIgnoreCase("image/png")) {
 			
-		    File convFile = new File(IMG_FOLDER_PATH + "drink_1.png");
+		    File convFile = new File(IMG_FOLDER_PATH + "/drink_" + beerId + ".png");
 		    file.transferTo(convFile);
 		    
 		    BufferedImage bufferedImage = ImageIO.read(convFile);
 			ImageIO.write(bufferedImage, "png", convFile);
 		} else {
-			file.transferTo(new File(IMG_FOLDER_PATH + "drink_2.png"));
+			file.transferTo(new File(IMG_FOLDER_PATH + "/drink_" + beerId + ".png"));
 		}
 		return "";
 	}
+	
+	@RequestMapping(value = "images/get/{beerId}", method = RequestMethod.GET)
+	public ResponseEntity<Resource> serveFile(@PathVariable("beerId") long beerId) {
+        Resource file = loadAsResource(getBeerImageFilename(beerId));
+        return ResponseEntity
+                .ok()
+                .body(file);
+	}
+	
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = IMG_FOLDER_PATH.resolve(filename);
+            System.out.println(file.toString());
+            Resource resource = new UrlResource(file.toUri());
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            else {
+                System.out.println("no file");
+            }
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+    private String getBeerImageFilename(long beerId) {
+    	return String.format("drink_%d.png", beerId);
+    }
 
 }
